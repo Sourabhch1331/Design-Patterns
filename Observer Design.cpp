90126dc77e7c54a999cf8dac69c8aa878a515f61
Sourabh Choudhary <100514971+Sourabhch1331@users.noreply.github.com>
2023-08-15 17:28:03 +0530

    Observer Design Pattern Init

---
+++ Observer Design.cpp
@@ -0,0 +1,125 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+
+// There are 2 main components in Observer design pattern
+// 1) Observable
+// 2) Observer
+
+// here the observer observe the observable, if there are any change in data in observable then
+// obervable notify all obeserver that it stores in a list
+
+
+// example: Walmart inteview problem
+// Design a Notify me Functionality
+
+// interface
+class NotificationAlertObserver {
+public:
+	virtual void update() = 0;
+};
+
+// Interface
+class StockObservable {
+public:
+	virtual void add(NotificationAlertObserver* observer) = 0;
+	virtual void remove(NotificationAlertObserver* observer) = 0;
+	virtual void notifySubscribers() = 0;
+	virtual int getStockCount() = 0;
+	virtual void setStockCount(int newStockCount) = 0;
+};
+
+class IPhoneObservable: public StockObservable {
+public:
+	list<NotificationAlertObserver*> *observerList;
+	int stockCount;
+
+	IPhoneObservable() {
+		observerList = new list<NotificationAlertObserver*>();
+		this->stockCount = 0;
+	}
+
+	void add(NotificationAlertObserver* observer) {
+		observerList->push_back(observer);
+	}
+
+	void remove(NotificationAlertObserver* observer) {
+		observerList->remove(observer);
+	}
+
+	void notifySubscribers() {
+		for (NotificationAlertObserver* observer : *observerList) {
+			observer->update();
+		}
+	}
+
+	void setStockCount(int newStockCount) {
+		if (stockCount == 0) {
+			notifySubscribers();
+		}
+		stockCount += newStockCount;
+	}
+
+	int getStockCount() {
+		return stockCount;
+	}
+};
+
+
+// Diffrent Observer implementing NotificationAlertObserver
+
+class EmailAlertObserver: public NotificationAlertObserver {
+public:
+	StockObservable* observable;
+	string emailId;
+
+	EmailAlertObserver(StockObservable* observable, string emailId) {
+		this->observable = observable;
+		this->emailId = emailId;
+	}
+
+	void update() {
+		sendMail();
+	}
+
+private:
+	void sendMail() {
+		cout << "Mail: Item is available in stock, Hurry up!\n";
+		cout << "Mail sent to :" << emailId << '\n';
+	}
+};
+
+class TextMessageObserver: public NotificationAlertObserver {
+public:
+	StockObservable* observable;
+	string phoneNumber;
+
+	TextMessageObserver(StockObservable* observable, string phoneNumber) {
+		this->observable = observable;
+		this->phoneNumber = phoneNumber;
+	}
+	void update() {
+		sendTextMessage();
+	}
+
+private:
+	void sendTextMessage() {
+		cout << "Message: Item is available in stock, Hurry up!\n";
+		cout << "Message sent to :" << phoneNumber << '\n';
+	}
+};
+
+
+
+int main() {
+	StockObservable* iPhoneObservable = new IPhoneObservable();
+
+	NotificationAlertObserver* customer1 = new EmailAlertObserver(iPhoneObservable, "sourabhchxyz@gmail.com");
+	NotificationAlertObserver* customer2 = new TextMessageObserver(iPhoneObservable, "881757xxxx");
+
+	iPhoneObservable->add(customer1);
+	iPhoneObservable->add(customer2);
+
+	iPhoneObservable->setStockCount(10);
+}
